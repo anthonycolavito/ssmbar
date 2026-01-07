63222785887c648c306a01e75a57728468e3e61b
Anthony Colavito <colavitoanthony@gmail.com>
2026-01-07 11:45:45 -0500

    Cleaned up tester file

--- tester.R
+++ tester.R
@@ -15,31 +15,3 @@ workers <- rbind(med_worker, low_worker)
 
 workers <- retired_worker(workers, tr2025, debugg = TRUE)
 
-first_yr <- 1960 + 21 #First earnings year
-last_yr <- 1960 + 119 #Last possible year alive (used for benefit amounts)
-
-years <- seq(first_yr, last_yr, 1)
-ages <- seq(21,119,1)
-
-worker <- data.frame(year = years, age = ages)
-
-real <- worker$real_earn
-avg <- sum(sort(real, decreasing = TRUE)[1:35]) / 35
-scalar <- 50000 / avg
-
-
-worker <- worker %>% left_join(sef %>% filter(worker == "raw") %>% select(age, factor),
-                               by = "age") %>%
-  mutate(
-    pi_age65 = gdp_pi[which(age==65)],
-    index = pi_age65 / gdp_pi,
-    nom_earn = factor * awi,
-    real_earn = nom_earn * index)
-
-med_worker <- med_worker %>% mutate(
-  test_earn = earnings * pi_age65 / gdp_pi
-)
-
-earn_vect <- med_worker$test_earn
-
-avg_test <- sum(sort(earn_vect, decreasing = TRUE)[1:35]) / 35
