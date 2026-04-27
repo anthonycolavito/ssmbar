90eba3a153ac1a295c62e34de5211ea1db5899fa
Anthony Colavito <colavitoanthony@gmail.com>
2026-04-27 08:35:46 -0400

    Wrote new helper function for removing assumption variables from data frame

--- R/general_helpers.R
+++ R/general_helpers.R
@@ -33,3 +33,30 @@ join_all_assumptions <- function(worker, assumptions) {
 floor_dime <- function(x) {
   floor(x * 10) / 10
 }
+
+remove_all_assumptions <- function(worker) {
+  # All columns needed by the benefit calculation pipeline
+  # - aime: awi, taxmax, qc_rec, qc_required, max_qc_per_year, max_dropout_years, min_comp_period, index_age_offset
+  # - pia: bp1, bp2, fact1, fact2, fact3, elig_age_retired, yoc_threshold, special_min_rate, min_yoc_for_special_min
+  # - cola: cola (year-by-year COLA percentage)
+  # - worker_benefit: nra, rf1, rf2, drc, max_drc_age
+  # - spousal_pia: s_pia_share
+  # - spouse_benefit: s_rf1, s_rf2
+  # - child_pia: child_pia_share
+  # - family_maximum: fm_bp1, fm_bp2, fm_bp3
+  # - ret: ret1, ret_phaseout_rate
+  
+  discard_cols <- c("awi", "taxmax", "qc_rec", "qc_required", "max_qc_per_year",
+                   "max_dropout_years", "min_comp_period", "index_age_offset",
+                   "bp1", "bp2", "fact1", "fact2", "fact3", "eea",
+                   "yoc_threshold", "special_min_rate", "min_yoc_for_special_min",
+                   "cola", "nra", "rf1", "rf2", "drc", "max_drc_age",
+                   "s_pia_share", "s_rf1", "s_rf2",
+                   "ret1", "ret_phaseout_rate")
+  
+  if (length(discard_cols) > 0) {  
+    worker <- worker %>% select(-all_of(discard_cols))
+  }
+  
+  return(worker)
+}
