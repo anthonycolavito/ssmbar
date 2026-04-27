efe914c4015730ea6623693fe83e26e2f9650726
Anthony Colavito <colavitoanthony@gmail.com>
2026-04-27 16:16:26 -0400

    more testing

--- R/test_script.R
+++ R/test_script.R
@@ -15,9 +15,12 @@ source("./R/special_min_pia.R")
 source("./R/worker_ben.R")
 source("./R/spousal.R")
 source("./R/calc_ben.R")
+source("./R/calc_tax.R")
 
-test_worker <- generate_retired_worker(sef2025, tr2025, birth_yr = 1960, claim_age = 62, type = "very_low")
-test_spouse <- generate_retired_worker(sef2025, tr2025, birth_yr = 1980, claim_age = 70, type = "high", spouse_id = "R-very_low-1960-44-62")
+test_worker <- generate_retired_worker(sef2025, tr2025, birth_yr = 1960, claim_age = 62, type = "very_low",spouse_id = "R-high-1980-44-70")
+test_spouse <- generate_retired_worker(sef2025, tr2025, birth_yr = 1980, claim_age = 70, type = "high")
 
 
-test_ben <- calc_ben(tr2025, test_worker, test_spouse, output = "detailed")
+test_ben <- calc_ben(tr2025, test_worker, test_spouse, output = "skinny")
+test_ben <- calc_tax(tr2025, test_ben)
+
